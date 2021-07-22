d3823ee3e9cd81389afddbfb8f0529f34a776ad9
RobertUriarte <robbyuriarte2@gmail.com>
2021-07-22 17:52:15 -0400

    Updated Pseudo Code

--- src/main/java/ucf/assignments/ErrorMessages.java
+++ src/main/java/ucf/assignments/ErrorMessages.java
@@ -1,25 +1,46 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 5 Solution
+ *  Copyright 2021 Robert Uriarte
+ */
 package ucf.assignments;
 
+//Set up imports
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
-
 import java.util.Optional;
 
 public class ErrorMessages {
     public static void displayDuplicateError(){
+        //Create alert
+        //Set alert title
+        //Set text
+        //make alert show and wait
+
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
         alert.setHeaderText("Serial Number already exist, please try again");
         Optional<ButtonType> result = alert.showAndWait();
     }
+
     public static void displaySerialFormatError(){
+        //Create alert
+        //Set alert title
+        //Set text
+        //make alert show and wait
+
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
         alert.setHeaderText("Incorrect serial number format");
         alert.setContentText("Format: XXXXXXXXXX of only letters and numbers.");
         Optional<ButtonType> result = alert.showAndWait();
     }
+
     public static void displaySerialLengthError(){
+        //Create alert
+        //Set alert title
+        //Set text
+        //make alert show and wait
+
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
         alert.setHeaderText("Serial Number entered is not of the correct length.");
@@ -28,6 +49,11 @@ public class ErrorMessages {
     }
 
     public static void displayMoneyError(){
+        //Create alert
+        //Set alert title
+        //Set text
+        //make alert show and wait
+
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
         alert.setHeaderText("Money value is entered in wrong format, please try again");
@@ -36,6 +62,11 @@ public class ErrorMessages {
     }
 
     public static void displayNameError(){
+        //Create alert
+        //Set alert title
+        //Set text
+        //make alert show and wait
+
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
         alert.setHeaderText("Name must be longer than 1 letter");
@@ -43,13 +74,24 @@ public class ErrorMessages {
     }
 
     public static void displayNameWarning(){
+        //Create alert
+        //Set alert title
+        //Set text
+        //make alert show and wait
+
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Warning");
         alert.setHeaderText("A name is only allowed a max length of 256");
         alert.setContentText("The name entered has been cut to 256 characters");
         Optional<ButtonType> result = alert.showAndWait();
     }
+
     public static void displayFileError(){
+        //Create alert
+        //Set alert title
+        //Set text
+        //make alert show and wait
+
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
         alert.setHeaderText("Error, file could not be saved");
@@ -57,6 +99,11 @@ public class ErrorMessages {
     }
 
     public static void displayFileNotFoundError(){
+        //Create alert
+        //Set alert title
+        //Set text
+        //make alert show and wait
+
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
         alert.setHeaderText("Error, file could not be loaded");
